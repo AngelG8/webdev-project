aaf9f0a545bdd0569f58ba289faf311907e70463
ZeguiJiang <monarchgui@gmail.com>
2023-06-25 13:52:40 -0700

    register screen modifyed

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const SERVER_API_URL = process.env.REACT_APP_API_BASE;
+// const SERVER_API_URL = process.env.REACT_APP_API_BASE;
+const SERVER_API_URL = "http://localhost:4000/api"
 const USERS_URL = `${SERVER_API_URL}/users`;
 
 const api = axios.create({ withCredentials: true });
@@ -25,8 +26,8 @@ export const updateUser = async (user) => {
     return response.data;
 };
 
-export const register = async ({ username, password }) => {
-    const response = await api.post(`${USERS_URL}/register`, { username, password });
+export const register = async ({ username, password, firstName, lastName, role, email , phone }) => {
+    const response = await api.post(`${USERS_URL}/register`, { username, password , firstName, lastName, role, email , phone });
     const user = response.data;
     return user;
 }

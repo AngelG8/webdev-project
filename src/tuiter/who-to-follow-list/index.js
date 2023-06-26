d910115454b5ac93e3c769525fcb8ddf67c79be1
angel <63884758+AngelG8@users.noreply.github.com>
2023-06-26 14:54:06 -0500

    Update index.js

--- src/tuiter/who-to-follow-list/index.js
+++ src/tuiter/who-to-follow-list/index.js
@@ -1,26 +1,26 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import WhoToFollowListItem from "./who-to-follow-list-item";
-import {useDispatch, useSelector} from "react-redux";
-import {findWhoToFollowListThunk} from "../services/who-thunks";
+import { useDispatch, useSelector } from "react-redux";
+import { findWhoToFollowListThunk } from "../services/who-thunks";
 
 const WhoToFollowList = () => {
-  const { whoToFollowList, loading } = useSelector(state => state.whoToFollowList);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(findWhoToFollowListThunk())
-  }, [])
-  return (
+    const { whoToFollowList, loading } = useSelector(state => state.whoToFollowList);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findWhoToFollowListThunk())
+    }, [])
+    return (
         <ul className="list-group">
             <li className="list-group-item">
                 <h3>Who to follow</h3>
             </li>
             {loading &&
                 <li className="list-group-item">
-                  Loading...
+                    Loading...
                 </li>
             }
             {
-              whoToFollowList.map(whoToFollow =>
+                whoToFollowList.map(whoToFollow =>
                     <WhoToFollowListItem
                         key={whoToFollow._id}
                         whoToFollow={whoToFollow} />

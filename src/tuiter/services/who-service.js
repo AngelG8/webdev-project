ba38121002f077b7fe8447b6cacd180cf14608fd
angel <63884758+AngelG8@users.noreply.github.com>
2023-06-26 14:27:01 -0500

    Removed hard coded API path

--- src/tuiter/services/who-service.js
+++ src/tuiter/services/who-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE = 'http://localhost:4000/api';
+const API_BASE = process.env.REACT_APP_API_BASE;
 const WHO_API = `${API_BASE}/users`;
 
 const api = axios.create({ withCredentials: true });
